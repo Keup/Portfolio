1415fb7c7a583d70aeeed6f95524304420f3e95d
Keup <julien.villard33@gmail.com>
2019-05-19 18:15:43 +0200

    Readme.md written, comments on JS and corrections

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,17 +1,14 @@
+// Materialize Jquery functionnality
+
 $(document).ready(function () {
   $('.sidenav').sidenav();
   $('.parallax').parallax();
   $('textarea#message').characterCounter();
   $('.scrollspy').scrollSpy();
   $('.modal').modal();
-  $('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true,
-  });
-  var autoplay = true;
-  setInterval(function () { if (autoplay) $('.carousel.carousel-slider').carousel('next'); }, 4500);
-  $('.carousel.carousel-slider').hover(function () { autoplay = false; }, function () { autoplay = true; });
 });
+
+// Captcha initialization
 
 grecaptcha.ready(function () {
   grecaptcha.execute('6Leh15cUAAAAAHcB7V4eZTnx65eTs109EuM2z9aH', { action: 'homepage' }).then(function (token) {
@@ -19,16 +16,10 @@ grecaptcha.ready(function () {
   });
 });
 
+// Form submit and treatment with feedback to the user
+
 $('#contactForm').submit(function () {
   event.preventDefault();
-  // // var data = $('#contactForm').serialize();
-  // var firstname = $('#firstname').val();
-  // var lastname = $('#lastname').val();
-  // var phone = $('#phone').val();
-  // var email = $('#email').val();
-  // var message = $('#message').val();
-  // var token = $('#token').val();
-
   $.ajax({
     type: "POST",
     url: "mail.php",
@@ -37,31 +28,16 @@ $('#contactForm').submit(function () {
     console.log(result);
     if (result == 'Success') {
       alert('Votre message a bien été envoyé.');
+      $('#contactForm')[0].reset();
     } else {
       alert("Erreur lors de l'envoi du message.");
     };
   }).fail(function () {
     alert("Erreur, merci de contacter l'administrateur.")
   }).always(function () {
-    $('#contactForm')[0].reset();
+    // $('#contactForm')[0].reset();
     grecaptcha.execute('6Leh15cUAAAAAHcB7V4eZTnx65eTs109EuM2z9aH', { action: 'homepage' }).then(function (token) {
       $('#token').val(token);
     });
   });
 });
-
-
-    // grecaptcha.ready(function(){
-    //   grecaptcha.execute('6Leh15cUAAAAAHcB7V4eZTnx65eTs109EuM2z9aH', {action: 'homepage'}).then(function(token){
-    //     $('#contactForm').prepend('<input type="hidden" id="token" name="token" value="' + token + '">');
-    //     $.post("mail.php",{firstname: firstname, lastname: lastname, phone: phone, email: email, message: message, token: token}),
-    //     function(result){
-    //       console.log(result);
-    //       if(result.success){
-    //         alert('Votre mail à bien été envoyé.')
-    //       } else {
-    //         alert('Spam')
-    //       }
-    //     }
-    //   });
-    // })
